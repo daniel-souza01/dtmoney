350c619a754e35328485e28bec25bcda8b6c9c56
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-10-22 12:00:29 -0300

    Criado hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,6 @@
-import { FormEvent, useState, useContext } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
-import { TransactionsContext } from '../../TransactionsContext'
-import { api } from '../../services/api'
+import { useTransactions } from '../../hooks/useTransactions'
 
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
@@ -14,7 +13,7 @@ interface newTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionModalProps){
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions()
 
   const [title, setTitle] = useState('')
   const [amount, setAmount] = useState(0)
